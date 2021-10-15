4d3881a4a77ffbf9564ccf31870905d2cf62f2db
Moofi App <moofinance.app@protonmail.com>
2021-10-15 11:12:14 +0200

    Added multicall contract

---
+++ scripts/deployMulticall.js
@@ -0,0 +1,25 @@
+const hardhat = require("hardhat");
+
+const ethers = hardhat.ethers;
+
+async function main() {
+  const deployer = await ethers.getSigner();
+
+  await hardhat.run("compile");
+
+  const Multicall = await ethers.getContractFactory("Multicall2");
+
+  console.log("Deploying Multicall...");
+
+  const multicall = await Multicall.deploy();
+  await multicall.deployed();
+
+  console.log("Multicall deployed to:", multicall.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
